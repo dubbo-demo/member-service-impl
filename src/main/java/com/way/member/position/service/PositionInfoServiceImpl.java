64d317bac04f9cc38458e1a2ad7f99aa32241a85
xuxinpei <649087712@qq.com>
2017-09-15 17:37:43 +0800

    优化

---
+++ src/main/java/com/way/member/position/service/PositionInfoServiceImpl.java
@@ -0,0 +1,19 @@
+package com.way.member.position.service;
+
+import com.way.member.position.dao.PositionInfoDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * 功能描述：定位信息ServiceImpl
+ *
+ * @Author：xinpei.xu
+ * @Date：2017/08/28 21:12
+ */
+@Service
+public class PositionInfoServiceImpl implements PositionInfoService {
+
+    @Autowired
+    private PositionInfoDao positionInfoDao;
+
+}
